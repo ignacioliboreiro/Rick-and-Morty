f3ad679c89981e16ce33b6638edde141a19ff916
ignaliboreiro <ignaliboreiro@gmail.com>
2023-12-06 11:48:38 -0300

    sbueindo async

--- backend/src/controllers/getCharById.js
+++ backend/src/controllers/getCharById.js
@@ -1,23 +1,25 @@
 const axios = require('axios');
 
-const getCharById = (req, res) => {
-    const { id } = req.params;
-    axios.get(`https://rickandmortyapi.com/api/character/${id}`)
-        .then(
-            response => {
-                if (response.data && response.data.id) {
-                    const { id, status, name, species, origin, image, gender } = response.data
-                    res.json({ id, status, name, species, origin, image, gender })
-                } else {
-                    res.status(404).json({ messege: 'error' })
-                }
-            }
-        )
-        .catch(
-            error => {
-                res.status(500).json({ message: error.message })
-            }
-        )
+
+
+
+
+const getCharById = async (req, res) => {
+try {
+    const characterId = req.params.id;
+    const {data}= await axios.get(`https://rickandmortyapi.com/api/character/${characterId}`)
+    const{
+        id, status, name, species, origin,image,gender,location
+    }= data;
+    const character = {
+        id, status, name, species, origin,image,gender,location
+    };
+    return character.name
+    ? res.json(character)
+    : res.status(404).send("Not Found")
+} catch (error) {
+    return res.status(500).send(error.message)
+}
 }
 
 module.exports = {getCharById}
